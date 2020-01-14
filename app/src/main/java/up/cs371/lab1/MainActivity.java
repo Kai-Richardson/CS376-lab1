293381b4b11411b02bc6ea526a73e1ddc3ab5dba
Kai Richardson <richardk22@up.edu>
2020-01-13 19:32:10 -0800

    Initial Commit

---
+++ app/src/main/java/up/cs371/lab1/MainActivity.java
@@ -0,0 +1,14 @@
+package up.cs371.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
